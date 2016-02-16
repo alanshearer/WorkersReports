3f442f856a0fafa6e8175fbecc02881a129a956f
alanshearer <enricobency@hotmail.com>
2016-02-16 21:06:52 +0100

    First commit

---
+++ WorkersReports/Models/WorkerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkersReports.Models
+{
+    public class WorkerData
+    {
+        public String MACAddress { get; set; }
+        public DateTime Time { get; set; }
+
+        public String Latitude { get; set; }
+        public String Longitude { get; set; }
+
+    }
+}
